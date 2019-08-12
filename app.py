878606b7d395ec85ff9c5cb16c9f7d41c17f16df
Jennifer Hikichi <jenniferhikichi@Jennifers-MacBook-Pro.local>
2019-08-11 20:30:05 -1000

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+  prog="Pyattck Scraping",
+  description="Tool for scraping the pyattack file",
+  prefix_chars="-"
+)
+
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+Cyber-cohort3 pyattck info scraping tool.  Tool for sourcing the actors and the tools that they use.
+This is a command line tool.
