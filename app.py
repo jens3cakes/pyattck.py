3763ee0aecf2938ac30a48e238ded5455b6f140f
Jennifer Hikichi <jenniferhikichi@Jennifers-MBP.home>
2019-08-12 18:18:26 -1000

    If status introduced to parse the args used.

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 import argparse
+import pyattck_tool
 
 parser = argparse.ArgumentParser(
   prog="Pyattck Scraping",
   description="Tool for scraping the pyattack file",
   prefix_chars="-"
 )
+
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-actor")
+group.add_argument("-tool")
+
+args = parser.parse_args()
+
+if args.tool:
+  pyattck_tool.find_tool()
+else:
+  pyattck_tool.find_actor()
+
+
+
 
 
