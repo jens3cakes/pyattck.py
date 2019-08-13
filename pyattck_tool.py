b366bb274f2406bb6b9a2d984c7d3bf7cc1b5f30
Jennifer Hikichi <jenniferhikichi@Jennifers-MBP.home>
2019-08-12 20:04:35 -1000

    readme instructions added

--- pyattck_tool.py
+++ pyattck_tool.py
@@ -15,20 +15,20 @@ def find_actor():
     if args.actor in actor.name:
       attack_info.write("Actor's name: " + actor.name)
       for tech in actor.techniques:
-        attack_info.write("\n" + "Technique or techniques used: " + tech.name)
+        attack_info.write("\n" + "Technique used: " + tech.name)
       for mal in actor.malware:
         attack_info.write("\n" + "Malware used: " + mal.name)
       for tools in actor.tools:
-        attack_info.write("\n" + "Tool or tools used: " + tools.name)
+        attack_info.write("\n" + "Tool used: " + tools.name)
   attack_info.close()
     
     
 def find_tool():
+  tool_report = open("attack_file4.txt", "a")
   for tool in attack.tools:
     if tool.name == args.tool:
-      print("this it is the " + args.tool)
+      tool_report.write("Tool name: " + tool.name)
       for actor in tool.actors:
-        print(actor.name)
+        tool_report.write("\nActor that use this tool: " + actor.name)
+  tool_report.close()
 
-find_actor() 
-find_tool()
--- readme.md
+++ readme.md
@@ -1,2 +1,13 @@
-Cyber-cohort3 pyattck info scraping tool.  Tool for sourcing the actors and the tools that they use.
-This is a command line tool.
+Cyber-cohort3 pyattck info scraping tool.
+**Requirements:** Python3
+You can use this command line tool to see information on the pyattack actor, such as what malware was used and what tool was used.
+`/.python3 app.py [-actor, -tool]`
+There are 2 arguments you can use
+  -actor = Name of the actor you would like information on.
+  -tool = Name of the tool used.
+
+The results are written to a file:
+  -actor >> attack_file3.txt
+  -tool >> attack_file4.txt
+
+Remember, snitches get stitches.
